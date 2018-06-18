405c29ae026c7d78745fbbaf3672f7439f7c8190
Brett Mosley <brett.mosley@accenturefederal.com>
2018-06-18 12:43:39 -0400

    added ds_store files and fixed generate function

--- bin/odata-service-downloader.js
+++ bin/odata-service-downloader.js
@@ -1,30 +1,37 @@
 #!/usr/bin/env node
-
-// 
-var wizard = require("../lib/command-line/wizard")
+// var wizard = require("../lib/command-line/wizard")
 var program = require('commander');
-var importa = require('../lib/import.js');
-var exporta = require('../lib/export.js');
+var service = require('../lib/service.js');
 var fs = require('fs');
 const path = require('path');
+
 program.
     version('0.1.0');
-// program
-//     .command('import <ODataServiceUri>')
-//     .alias('i')
-//     .description('Import Metadata and Data from Service Url') 
-//     .option('-d, --dest [destinationPath]', "Destination Path", "./")
-//     .action(importServiceUri);
-// program   
-//     .command('generate <ExcelSpreadsheetFilename>')
-//     .alias('g')
-//     .description('Generate Metadata and Data from Excel Spreadsheet') 
-//     .option('-d, --dest [destinationPath]', "Destination Path", "./odataservice-dump")
-//     .action(generateMetadataFromExcel);
+program
+    .command('import <ServiceUrl>')
+    .alias('i')
+    .description('This command imports metadata and collections from a ODATA service url.\n') 
+    .option('-d, --dest [destinationPath]', "Destination Path", "./")
+    .action((serviceurl, args)=>{
+        var dest = [args.dest,path.basename(serviceurl)].join("");
+        service.downloadDataFromODataServiceUrl(serviceurl, dest);
+    });
+program   
+    .command('generate <filename.xlsx|xls>')
+    .alias('g')
+    .description('This command generates metadata and data from a excel spreadsheet.\n') 
+    .option('-d, --dest [destinationPath]', "Destination Path", "./")
+    .action((filename, args) => {
+        var basename = path.basename(filename).split(".")[0];
+        var dest = [args.dest, [basename+"-metadata", 'xml'].join(".")].join("");
+        service.generateMetadata(filename, dest, function(){
+            console.log("\n\Generated data and metadata file Succesfully! \n\tDatafile= %s  \n\tMetadata file= %s\n", filename, dest);
+        });
+    });
 program   
     .command('xltojson <filname.xlsx|xls>')
     .alias('x')
-    .description('This command generates a json file from a excel spreadsheet.') 
+    .description('This command generates a json file from a excel spreadsheet.\n') 
     .option('-d, --dest [destinationPath]', "Destination Path", "./")
     .action((filename, args) => {
         if (!fs.existsSync(filename)) {
@@ -32,21 +39,10 @@ program
         }
         var basename = path.basename(filename).split(".")[0];
         var destfilename = [args.dest, [basename, 'json'].join(".")].join("/");
-        exporta.convertFromExcel(filename, destfilename);
+        service.excelToJson(filename, destfilename, (response) => {
+            console.log("\n\tExported %s to %s successfully\n", filename, destfilename);
+        });
     });
 program.parse(process.argv);
 
 //wizard.run();
-function importServiceUri(endpointUrl, args){
-    console.log("Import");
-    console.log(endpointUrl);
-    console.log(args.dest);
-    importa.downloadDataFromODataServiceUrl(endpointUrl, args.dest);
-}
-function generateMetadataFromExcel(excelFilename, args){
-    console.log("Generate");
-    console.log(excelFilename);
-    console.log(args.dest);
-    exporta.generateMetadata();
-}
-
