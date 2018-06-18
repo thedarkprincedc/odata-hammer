584585e610afd17b68d0ad9074d85bb07375cda5
Brett Mosley <brett.mosley@accenturefederal.com>
2018-06-18 10:44:09 -0400

    fixed excel to json functionality

--- bin/odata-service-downloader.js
+++ bin/odata-service-downloader.js
@@ -5,24 +5,38 @@ var wizard = require("../lib/command-line/wizard")
 var program = require('commander');
 var importa = require('../lib/import.js');
 var exporta = require('../lib/export.js');
-
+var fs = require('fs');
+const path = require('path');
 program.
     version('0.1.0');
-program
-    .command('import <ODataServiceUri>')
-    .alias('i')
-    .description('Import Metadata and Data from Service Url') 
-    .option('-d, --dest [destinationPath]', "Destination Path", "./")
-    .action(importServiceUri);
+// program
+//     .command('import <ODataServiceUri>')
+//     .alias('i')
+//     .description('Import Metadata and Data from Service Url') 
+//     .option('-d, --dest [destinationPath]', "Destination Path", "./")
+//     .action(importServiceUri);
+// program   
+//     .command('generate <ExcelSpreadsheetFilename>')
+//     .alias('g')
+//     .description('Generate Metadata and Data from Excel Spreadsheet') 
+//     .option('-d, --dest [destinationPath]', "Destination Path", "./odataservice-dump")
+//     .action(generateMetadataFromExcel);
 program   
-    .command('generate <ExcelSpreadsheetFilename>')
-    .alias('g')
-    .description('Generate Metadata and Data from Excel Spreadsheet') 
-    .option('-d, --dest [destinationPath]', "Destination Path", "./odataservice-dump")
-    .action(generateMetadataFromExcel);
+    .command('xltojson <filname.xlsx|xls>')
+    .alias('x')
+    .description('This command generates a json file from a excel spreadsheet.') 
+    .option('-d, --dest [destinationPath]', "Destination Path", "./")
+    .action((filename, args) => {
+        if (!fs.existsSync(filename)) {
+           // file not found exception
+        }
+        var basename = path.basename(filename).split(".")[0];
+        var destfilename = [args.dest, [basename, 'json'].join(".")].join("/");
+        exporta.convertFromExcel(filename, destfilename);
+    });
 program.parse(process.argv);
-wizard.run();
 
+//wizard.run();
 function importServiceUri(endpointUrl, args){
     console.log("Import");
     console.log(endpointUrl);
